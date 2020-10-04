0c039f9a7cb38029360cdb83c16f4b47209917b5
Vincent <vrdphone@gmail.com>
2020-10-04 11:07:10 -0500

    fixed error bug because of console.log

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
@@ -25,14 +22,24 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
-// app.post('/api/notes', (req, res) => {
-//     const noteArray = [];
-//     let newNote = req.body;
-//     noteArray.push(newNote);
-//     console.log(noteArray);
-//     res.json(noteArray);
-//     // console.log(newNote);
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.post('/api/notes', (req, res) => {
+    let noteArray = [];
+    let newNote = req.body;
+    fs.readFile(__dirname + "/db/db.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        noteArray = JSON.parse(data);
+        noteArray.push(newNote);
+
+        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(noteArray), 'utf-8', err => {
+            if (err) throw err;
+            res.end();
+        });
+    });
+});
 
 
 
